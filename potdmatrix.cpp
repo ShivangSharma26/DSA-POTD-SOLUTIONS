3d906f02950e5e37d53de4f3385e0817e3aae108
ShivangSharma26 <iamluckysharma99@gmail.com>
2026-01-07 23:52:48 +0530

    yo

---
+++ potdmatrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int mini=INT_MAX;
+        int cnt=0;
+        
+
+        int sum=0;
+        int n=matrix.size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                mini=min(mini,abs(matrix[i][j]));
+                if(matrix[i][j]>0)sum+=matrix[i][j];
+                else if(matrix[i][j]<0){
+                 cnt++;
+                 sum+=abs(matrix[i][j]);
+                 }
+                 
+                 
+            }
+        }
+        if(cnt%2==0) return sum;
+        else return sum-mini;
+    
+    }
+};
